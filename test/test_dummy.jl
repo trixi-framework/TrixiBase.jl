ec408acaa04c531b87265816d0f7b3f393da1fe9
Michael Schlottke-Lakemper <michael@sloede.com>
2024-01-16 10:50:03 +0100

    Fix tests

--- test/test_dummy.jl
+++ test/test_dummy.jl
@@ -6,7 +6,7 @@ using TrixiBase
 @testset verbose=true showtiming=true "test_dummy.jl" begin
 
 @testset verbose=true showtiming=true "greet" begin
-    @test_nowarn TestDummy.greet()
+    @test_nowarn TrixiBase.greet()
 end
 
 end # @testset "test_dummy.jl"
