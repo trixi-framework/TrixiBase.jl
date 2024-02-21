6313b9374b9fb17b73c020b2380c0374edd4c651
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2024-02-21 10:35:46 +0100

    precompile trixi_include (#15)

--- src/TrixiBase.jl
+++ src/TrixiBase.jl
@@ -4,4 +4,11 @@ include("trixi_include.jl")
 
 export trixi_include
 
+function _precompile_manual_()
+    @assert Base.precompile(Tuple{typeof(trixi_include), String})
+    return nothing
+end
+
+_precompile_manual_()
+
 end # module TrixiBase
