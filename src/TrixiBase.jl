f42854f6afeb391130833a6f1ba09eded786c06a
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-06-07 13:04:42 +0200

    Add `timer()` and `@trixi_timeit`

--- src/TrixiBase.jl
+++ src/TrixiBase.jl
@@ -1,8 +1,12 @@
 module TrixiBase
 
+using TimerOutputs: TimerOutput, TimerOutputs
+
 include("trixi_include.jl")
+include("trixi_timeit.jl")
 
 export trixi_include
+export @trixi_timeit, timer, timeit_debug_enabled, disable_debug_timings
 
 function _precompile_manual_()
     @assert Base.precompile(Tuple{typeof(trixi_include), String})
