57ea9b15c84aebaf156596269e7f0448311a5867
Valentin Churavy <v.churavy@gmail.com>
2025-06-29 13:23:08 +0200

    use a package callback to reset the cache

--- src/mpi.jl
+++ src/mpi.jl
@@ -2,14 +2,29 @@
 # versions if MPI.jl is loaded to avoid letting TrixiBase.jl
 # depend explicitly on MPI.jl.
 
-# XXX: There is an order problem here:
+# There is an order problem here, with naive caching:
 # TrixiBase.mpi_isparallel() -> false
 # using MPI
 # TrixiBase.mpi_isparallel() -> false
 # Since we cached the check for MPI being loaded.
+# We use an internal Julia feature "package_callbacks" to reset the cache when MPI is loaded.
 
 # XXX: This should be replaced with OncePerProcess or similar in 1.12
 const __MPI__AVAILABLE__ = Ref{Union{Nothing, Bool}}(nothing)
+
+function reset_mpi_available(mod::Base.PkgId)
+    if mod == Base.PkgId(Base.UUID("da04e1cc-30fd-572f-bb4f-1f8673147195"), "MPI")
+        __MPI__AVAILABLE__[] = nothing
+    end
+    return nothing
+end
+function __init__()
+    # Future-proof if Julia Base ever removes this code
+    if isdefined(Base, :package_callbacks)
+        push!(Base.package_callbacks, reset_mpi_available)
+    end
+end
+
 function __mpi__available()
     val = __MPI__AVAILABLE__[]
     if val === nothing
