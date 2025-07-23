1fdbb32d89f63a73be44b975e01ada879911d02b
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2025-07-23 21:29:21 +0200

    Use `at-trixi_testset` from TrixiTest.jl (#50)
    * use at-trixi_testset from TrixiTest.jl
    
    * bump minimal TrixiTest.jl version

--- test/test_util.jl
+++ test/test_util.jl
@@ -1,35 +1,5 @@
 # All `using` calls are in this file, so that one can run any test file
 # after running only this file.
 using Test: @test, @testset, @inferred
-using TrixiTest: @trixi_test_nowarn
+using TrixiTest: @trixi_test_nowarn, @trixi_testset
 using TrixiBase
-
-"""
-    @trixi_testset "name of the testset" #= code to test #=
-
-Similar to `@testset`, but wraps the code inside a temporary module to avoid
-namespace pollution.
-"""
-macro trixi_testset(name, expr)
-    @assert name isa String
-
-    mod = gensym()
-
-    # TODO: `@eval` is evil
-    quote
-        @eval module $mod
-        using Test
-        using TrixiBase
-
-        # We also include this file again to provide the definition of
-        # the other testing macros. This allows to use `@trixi_testset`
-        # in a nested fashion and also call `@test_nowarn_mod` from
-        # there.
-        include(@__FILE__)
-
-        @testset verbose=true $name $expr
-        end
-
-        nothing
-    end
-end
